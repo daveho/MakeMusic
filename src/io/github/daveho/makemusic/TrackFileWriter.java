cd2c633da63f8dc6322da7027615cffe51219995
David Hovemeyer <david.hovemeyer@gmail.com>
2015-03-01 11:27:55 -0500

    starting writer for track data

---
+++ src/io/github/daveho/makemusic/TrackFileWriter.java
@@ -0,0 +1,13 @@
+package io.github.daveho.makemusic;
+
+import io.github.daveho.makemusic.data.TrackData;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class TrackFileWriter {
+	public void write(Writer w, TrackData trackData) throws IOException {
+		IMessageGeneratorData msgGenData = trackData.getMessageGeneratorData();
+		
+	}
+}
